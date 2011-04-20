83f6befd4e5df604adcc27d451f481087eb2f310
Romel Campbell <romel.campbell@tarcml28297.regeneron.regn.com>
2011-04-20 15:51:13 -0400

    Initial commit

---
+++ apps/todos/models/task.js
@@ -0,0 +1,22 @@
+// ==========================================================================
+// Project:   Todos.Task
+// Copyright: ©2011 My Company, Inc.
+// ==========================================================================
+/*globals Todos */
+
+/** @class
+
+  (Document your Model here)
+
+  @extends SC.Record
+  @version 0.1
+*/
+Todos.Task = SC.Record.extend(
+/** @scope Todos.Task.prototype */ {
+
+  // TODO: Add your own code here.
+
+   isDone: SC.Record.attr(Boolean),
+   description: SC.Record.attr(String)
+
+}) ;
---
+++ tmp/debug/build/static/todos/en/current/source/controllers/tasks.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   Todos.tasksController
+// Copyright: ©2011 My Company, Inc.
+// ==========================================================================
+/*globals Todos */
+
+/** @class
+
+  (Document Your Controller Here)
+
+  @extends SC.ArrayController
+*/
+Todos.tasksController = SC.ArrayController.create(
+/** @scope Todos.tasksController.prototype */ {
+
+  // TODO: Add your own code here.
+
+}) ;
+; if ((typeof SC !== 'undefined') && SC && SC.scriptDidLoad) SC.scriptDidLoad('todos');
